1956f0caf3aa8354f43f3bdf006b692510a30a55
PanagiotisVard <panagiotis.vardaris@uoi.gr>
2023-07-06 12:20:03 +0300

    Made the about and home page load dynamic data

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 const path = require('path')
 const express = require('express')
 
+
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
 
+app.set('view engine','hbs')
 app.use(express.static(publicDirectoryPath))
+
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title: 'Weather',
+        name: 'Panos'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title: 'About page',
+        name: 'Panos'
+    })
+})
 
 app.get('/weather', (req, res)=>{
     res.send([{
