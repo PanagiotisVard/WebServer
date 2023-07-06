f1053a7fe782c9401acfeea363c4c889fd391542
PanagiotisVard <panagiotis.vardaris@uoi.gr>
2023-07-06 14:08:25 +0300

    Advanced templates -> Done!

--- src/app.js
+++ src/app.js
@@ -1,25 +1,37 @@
 const path = require('path')
 const express = require('express')
-
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname,'../public')
 
+
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'..//templates/partials')
+
+//Setup handlebars engine and views location
 app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+//Set up static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 
 app.get('',(req,res)=>{
     res.render('index',{
         title: 'Weather',
-        name: 'Panos'
+        creator: 'Made by Panos'
     })
 })
 
 app.get('/about',(req,res)=>{
     res.render('about',{
         title: 'About page',
-        name: 'Panos'
+        creator: 'Made by Panos'
+
     })
 })
 
@@ -32,8 +44,17 @@ app.get('/weather', (req, res)=>{
 
 app.get('/help', (req, res)=>{
     res.render('help',{
-        phone: '6999999999'})
+        phone: '6999999999',
+        title: 'Help',
+        creator: 'Made by Panos'
+
+    })
 })
+
+app.get('*',(req,res)=>{
+    res.send('Error 404 page')
+})
+
 
 app.listen(3000, ()=>{
     console.log('Server is up on port 3000.')
