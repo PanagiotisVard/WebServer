b4c00ab166688514a100b897779e727fc3469af5
PanagiotisVard <panagiotis.vardaris@uoi.gr>
2023-07-06 17:49:26 +0300

    test

--- src/app.js
+++ src/app.js
@@ -45,6 +45,9 @@ app.get('/weather', (req, res)=>{
         })
     }
 
+
+
+    //zxczxcz
     geocode(req.query.address, (error,{latitude,longitude,location}= {})=>{
         if(error){
             return res.send({error})
